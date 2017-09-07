e93d7115b50e9048993024596be1329bac817615
Danilo Gomes <danilo_rs_gomes@hotmail.com>
2017-09-07 20:25:29 +0100

    Initial structure. code sample from audacity.

--- virtual-tourist/Constants.swift
+++ virtual-tourist/Constants.swift
@@ -16,4 +16,54 @@ struct Constants {
         static let showAlbum = "showAlbum"
     }
     
+    
+    // MARK: Flickr
+    // The code below was extracted from the Udemy project FlickrFinder.
+    
+    struct Flickr {
+        static let APIScheme = "https"
+        static let APIHost = "api.flickr.com"
+        static let APIPath = "/services/rest"
+    }
+    
+    
+    struct FlickrParameterKeys {
+        static let Method = "method"
+        static let APIKey = "api_key"
+        static let GalleryID = "gallery_id"
+        static let Extras = "extras"
+        static let Format = "format"
+        static let NoJSONCallback = "nojsoncallback"
+        static let SafeSearch = "safe_search"
+        static let Text = "text"
+        static let BoundingBox = "bbox"
+        static let Page = "page"
+    }
+    
+    
+    struct FlickrParameterValues {
+        static let SearchMethod = "flickr.photos.search"
+        static let APIKey = "6e72b83e4dc79e9617c11dd9202fa0ff"
+        static let ResponseFormat = "json"
+        static let DisableJSONCallback = "1" /* 1 means "yes" */
+        static let GalleryPhotosMethod = "flickr.galleries.getPhotos"
+        static let GalleryID = "5704-72157622566655097"
+        static let MediumURL = "url_m"
+        static let UseSafeSearch = "1"
+    }
+    
+    
+    struct FlickrResponseKeys {
+        static let Status = "stat"
+        static let Photos = "photos"
+        static let Photo = "photo"
+        static let Title = "title"
+        static let MediumURL = "url_m"
+        static let Pages = "pages"
+        static let Total = "total"
+    }
+    
+    struct FlickrResponseValues {
+        static let OKStatus = "ok"
+    }
 }
