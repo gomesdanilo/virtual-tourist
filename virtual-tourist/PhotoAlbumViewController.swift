77dbf942de041aba9588c9fcd04d193dd5a8b167
Danilo Gomes <danilo_rs_gomes@hotmail.com>
2017-09-08 19:53:22 +0100

    core data initial structure

--- virtual-tourist/PhotoAlbumViewController.swift
+++ virtual-tourist/PhotoAlbumViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class PhotoAlbumViewController: UIViewController {
 
@@ -19,7 +20,6 @@ class PhotoAlbumViewController: UIViewController {
     
     let server = FLKRClient.sharedInstance()
     var coordinates : CLLocationCoordinate2D?
-    var pictures : [FLKRPicture]?
     var imageData : [UIImage?]?
     
     
@@ -75,21 +75,21 @@ class PhotoAlbumViewController: UIViewController {
     
     func downloadPicture(indexPath: IndexPath){
         
-        if let pics = pictures {
-            let pic = pics[indexPath.row]
-            if let url = pic.url {
-                FLKRClient.sharedInstance().downloadPicture(url: url, completionHandler: { (url, error) in
-                    
-                    guard let url = url else {
-                        return
-                    }
-                    
-                    let image = self.imageDataFromDisk(url: url)
-                    self.imageData![indexPath.row] = image
-                    self.collectionView?.reloadItems(at: [indexPath])
-                })
-            }
-        }
+//        if let pics = pictures {
+//            let pic = pics[indexPath.row]
+//            if let url = pic.url {
+//                FLKRClient.sharedInstance().downloadPicture(url: url, completionHandler: { (url, error) in
+//                    
+//                    guard let url = url else {
+//                        return
+//                    }
+//                    
+//                    let image = self.imageDataFromDisk(url: url)
+//                    self.imageData![indexPath.row] = image
+//                    self.collectionView?.reloadItems(at: [indexPath])
+//                })
+//            }
+//        }
     }
     
     func imageForCell(indexPath: IndexPath) -> UIImage?{
@@ -106,14 +106,14 @@ class PhotoAlbumViewController: UIViewController {
     
     func enableNoPicturesMode(){
         // Error / No pictures
-        self.pictures = []
+        //self.pictures = []
         self.imageData = []
         self.showNoPicturesLabel()
         self.collectionView?.reloadData()
     }
     
     func loadScreenWithPictures(_ pictures : [FLKRPicture]){
-        self.pictures = pictures
+        //self.pictures = pictures
         self.imageData = Array<UIImage?>(repeating: nil, count: pictures.count)
         self.showPicturesGrid()
         self.collectionView?.reloadData()
@@ -142,27 +142,36 @@ class PhotoAlbumViewController: UIViewController {
 extension PhotoAlbumViewController : UICollectionViewDataSource, UICollectionViewDelegate {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+        if let fc = fetchedResultsController {
+            return (fc.sections?.count)!
+        } else {
+            return 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if let pics = pictures {
-            return pics.count
+        if let fc = fetchedResultsController {
+            return fc.sections![section].numberOfObjects
+        } else {
+            return 0
         }
-        return 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photo", for: indexPath) as! PhotoAlbumViewCell
         
-        let image = imageForCell(indexPath: indexPath)
-        if(image != nil){
-            cell.updatePicture(image: image!)
-        } else {
-            cell.startLoading()
-            downloadPicture(indexPath: indexPath)
-        }
-        
+        let photo = fetchedResultsController!.object(at: indexPath) as! Photo
+        print("rendering phoro: ", photo.url!)
+//        
+//        
+//        let image = imageForCell(indexPath: indexPath)
+//        if(image != nil){
+//            cell.updatePicture(image: image!)
+//        } else {
+//            cell.startLoading()
+//            downloadPicture(indexPath: indexPath)
+//        }
+//        
         return cell
     }
 }
